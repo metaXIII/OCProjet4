8d5604cd3caf6687afa3d0cd8b1350c3b8146d27
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-10-26 12:20:39 +0200

    First step of HTML

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: MetaXIII
+ * Date: 26/10/2018
+ * Time: 10:48
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog pour écrivain</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+<script src="./js/jquery-3.3.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
+</body>
+</html>
