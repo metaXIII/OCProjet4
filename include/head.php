2c35d342961067bd4efe679a908b913558d23862
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-05 23:48:44 +0100

    Modify head for ROOT support

--- include/head.php
+++ include/head.php
@@ -14,7 +14,7 @@
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="<?= WEBROOT ?>">Accueil</a>
+        <a class="navbar-brand" href="<?= ROOT ?>">Accueil</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
