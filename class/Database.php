9d843bdebf7b1e90b00e517609efbf6f1cb413ea
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-07 22:28:04 +0100

    Modify Database due to error

--- class/Database.php
+++ class/Database.php
@@ -16,9 +16,10 @@ class Database
 
         if (self::$pdo === null) {
             try {
-                self::$pdo = new \PDO("mysql:host=" . $hostname . ";dbname = " .
-                    $dbname . ";charset = " . $charset, $username, $password);
-            } catch (Exception $e) {
+                self::$pdo = new \PDO("mysql:host=$hostname.;dbname=$dbname;charset=$charset",
+                $username, $password);
+                self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (\Exception $e) {
                 die("Erreur" . $e->getMessage());
             }
         }
