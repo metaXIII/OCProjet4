0d48f95051ce71637322df93831e0713f9070e8c
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-07 22:26:55 +0100

    Add first view of website

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: MetaXIII
- * Date: 26/10/2018
- * Time: 10:48
- */
+use metaxiii\blog\Autoloader;
+use metaxiii\blog\Database;
+use metaxiii\blog\Router;
 
-include "include/head.php";
-include "include/header.php";
+require 'class/Database.php';
+include 'lib/list.php';
+require 'class/Autoloader.php';
+
+Autoloader::register();
+
+
+//$form = new Form();
+//echo $form->input("aze");
+//echo $form->input("aze");
+//echo $form->submit();
+
+
+//$db = Database::getPdo();
+//$request = "SELECT * from article";
+//$result = $db->query($request);
+
+//var_dump($result->fetch());
+//
+$router = new Router($_GET['url']);
+$router->get('/', function () {
+    include "view/accueil.php";
+});
+$router->get('/:slug', function ($slug) {
+    include ('view/article.php');
+});
+//$router->get('/posts', function () {
+//    echo "tous les articles";
+//});
+//$router->get('/404', function () {
+//   echo "there was an error";
+//});
+//
+$router->run();
 ?>
 
-<?php include "include/footer.php"; ?>
-
-
+<?php //include "lib/debug.php"; ?>
+<?php //include "include/footer.php"; ?>
