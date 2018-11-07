f4cd09c5c4909eb393cbd3fa1d0647972259dea7
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-07 22:29:28 +0100

    get surrond on class form

--- class/Form.php
+++ class/Form.php
@@ -5,16 +5,22 @@ namespace metaxiii\blog;
 class Form
 {
 
-    public $surrond = 'p';
+    public $surrond = 'div';
+    private $data;
+
+    public function __construct($data = array())
+    {
+        $this->data = $data;
+    }
 
     private function surrond($html)
     {
-        return "<{$this->surrond}>{$html}</{$this->surrond}>";
+        return "<{$this->surrond} class='form-group'>{$html}</{$this->surrond}>";
     }
 
     public function input($data)
     {
-        return $this->surrond("<input type='text' placeholder='$data' name='$data'>", 'p');
+        return $this->surrond("<input type='text' name='$data' class='form-control'>");
     }
 
     public function submit()
