106370c2b4bafa1109f8fcbd0a552891bf8b5fc2
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-07 22:32:31 +0100

    Modify constant and change display init for hide/show error

--- lib/constants.php
+++ lib/constants.php
@@ -1,5 +1,8 @@
 <?php
-
+//Désactive l'affichage d'erreur
+//ini_set('display_errors','off');
+error_reporting(E_ALL);
+//Création du ROOT
 $directory = basename(dirname(dirname(__FILE__)));
 $url = explode($directory, $_SERVER['REQUEST_URI']);
 if (count($url) == 1)
