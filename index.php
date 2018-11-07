6598e89e62130d3a88ef3a1ae385a84db76645e1
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-08 00:56:11 +0100

    Put code in route.php for clean file

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 use metaxiii\blog\Autoloader;
-use metaxiii\blog\Database;
-use metaxiii\blog\Router;
 
 require 'class/Database.php';
 include 'lib/list.php';
@@ -15,28 +13,7 @@ Autoloader::register();
 //echo $form->input("aze");
 //echo $form->submit();
 
-$router = new Router($_GET['url']);
-$router->get('/', function () {
-    include "view/accueil.php";
-});
-$router->get("/error", function () {
-   include "view/error404.php";
-});
-$router->get('/login', function () {
-    include "view/login.php";
-});
-$router->get('/:slug', function () {
-    include ('view/article.php');
-});
-
-//$router->get('/posts', function () {
-//    echo "tous les articles";
-//});
-//$router->get('/404', function () {
-//   echo "there was an error";
-//});
-//
-$router->run();
+require "lib/routes.php";
 ?>
 
 <?php //include "lib/debug.php"; ?>
---
+++ lib/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use metaxiii\blog\Router;
+
+$router = new Router($_GET['url']);
+$router->get('/', function () {
+    include "view/accueil.php";
+});
+$router->get("/error", function () {
+    include "view/error404.php";
+});
+$router->get('/login', function () {
+    include "view/login.php";
+});
+$router->get('/:slug', function () {
+    include ('view/article.php');
+});
+
+$router->run();
