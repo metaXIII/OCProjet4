63bd119fab627476178ceacdb83033fcf97ce1a9
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-09 14:56:48 +0100

    Implement add method on commentDAO

--- class/CommentDAO.php
+++ class/CommentDAO.php
@@ -16,4 +16,30 @@ class CommentDAO
     {
         return $comment = $this->db->query("SELECT * FROM comment WHERE show_comment LIKE '0'")->rowCount();
     }
+
+    public function add($data = array())
+    {
+        $name = "";
+        $commentaire = "";
+        $url = 0;
+        var_dump($data);
+        extract($data);
+        $name = htmlspecialchars(strip_tags($name));
+        $commentaire = htmlspecialchars(strip_tags($commentaire));
+        $url = (int)$url;
+        if (!empty($name) && !empty($commentaire)) {
+            $query = "INSERT INTO comment (user, content, id_article)
+                      VALUES (:user, :content, :id)";
+            $req = $this->db->prepare($query);
+            $req->bindValue(":user", $name);
+            $req->bindValue(":content", $commentaire);
+            $req->bindValue(":id", $url);
+            $req->execute();
+        }
+    }
+
+    public function getComment($data)
+    {
+
+    }
 }
