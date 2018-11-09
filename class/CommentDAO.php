ab2b58a2f4fa06751b0b439136630de7b2aa922e
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-09 13:10:46 +0100

    implements method of MethodDAO for moderate comment

--- class/CommentDAO.php
+++ class/CommentDAO.php
@@ -2,6 +2,18 @@
 
 namespace metaxiii\blog;
 
-class CommentDAO {
+class CommentDAO
+{
 
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getPdo();
+    }
+
+    public function checkComment()
+    {
+        return $comment = $this->db->query("SELECT * FROM comment WHERE show_comment LIKE '0'")->rowCount();
+    }
 }
