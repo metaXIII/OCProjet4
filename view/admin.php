608b9b791150f675e92f4ef7b3e8fbe1e4053673
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-09 13:14:16 +0100

    add admin page for custom check

--- view/admin.php
+++ view/admin.php
@@ -1,3 +1,21 @@
 <?php
+
+use metaxiii\blog\CommentDAO;
+
 require "include/head.php";
-echo "page d'administration";
+/**
+ * Must return flash if needed
+ */
+checkComment();
+?>
+
+<div class="container mt-5 pt-5">
+    <div class="row">
+        <a href="admin/article" class="custom-link-admin offset-2 col-4 bloc btn btn-primary">
+            <span class="bloc-text">Articles</span>
+        </a>
+        <a href="admin/commentaires" class="custom-link-admin offset-2 col-4 bloc btn btn-primary">
+            <span class="bloc-text">Commentaires</span>
+        </a>
+    </div>
+</div>
