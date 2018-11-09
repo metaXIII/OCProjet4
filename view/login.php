9be82ef1a742516513af7110393c03be59d443c3
Lehchibi Gaël <lehchibi.gael@gmail.com>
2018-11-09 13:14:34 +0100

    replace form submit in view

--- view/login.php
+++ view/login.php
@@ -4,15 +4,13 @@ $_SESSION['Auth'] = 0;
 require "include/head.php";
 
 use metaxiii\blog\Form;
-use metaxiii\blog\User;
 use metaxiii\blog\UserDAO;
 
-if (isset($_POST) && !empty($_POST)){
+if (isset($_POST) && !empty($_POST)) {
 
     foreach ($_POST as $key => $value) {
         $_POST[$key] = strip_tags(htmlspecialchars($value));
     }
-
     $user = new UserDAO();
     $user->get($_POST);
 }
@@ -21,13 +19,13 @@ $form = new Form();
 ?>
 
 
-    <div class="offset-lg-3 offset-2 col-lg-6 col-10 mt-5 bg-grey ml-auto mr-auto p-5">
-        <form action="" method="post">
-            <?= $form->input("name"); ?>
-            <?= $form->password("password"); ?>
-            <?= $form->submit(); ?>
-        </form>
-    </div>
+<div class="offset-lg-3 offset-2 col-lg-6 col-10 mt-5 bg-grey ml-auto mr-auto p-5">
+    <form action="" method="post">
+        <?= $form->input("name"); ?>
+        <?= $form->password("password"); ?>
+        <?= $form->submit("Se connecter"); ?>
+    </form>
+</div>
 <?php
 require "include/footer.php";
 ?>
